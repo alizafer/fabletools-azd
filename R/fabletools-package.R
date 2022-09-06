0d412a9e6519f9c30462e7bc272c839b51b7468c
alizafer <66441604+alizafer@users.noreply.github.com>
2022-09-06 16:02:05 +0300

    Update fabletools-package.R

--- R/fabletools-package.R
+++ R/fabletools-package.R
@@ -8,6 +8,7 @@ globalVariables(".")
 #' @import rlang
 #' @import vctrs
 #' @import tsibble
+#' @import liureg
 #' @importFrom dplyr mutate transmute summarise filter select rename group_by ungroup groups group_data 
 #' @importFrom dplyr full_join anti_join left_join semi_join
 #' @importFrom dplyr dplyr_row_slice dplyr_col_modify dplyr_reconstruct
